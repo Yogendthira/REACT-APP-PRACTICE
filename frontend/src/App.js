804ce7f1f041d6b01fe780a9bb0f89bf4734bfb8
Yogendthira <yogendthira.official@gmail.com>
2026-01-08 13:11:12 +0530

    fix: convert frontend submodule to regular folder for deployment

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+
+
+
+  return (
+
+    <div>
+      <h1>HELLO SOLAR SYSTEM </h1>
+      <p>This is my first REACT APP</p>\
+
+      <img src="/images/textbg1.jpg" className="position1" />
+ <button className="shapesbutton">SHAPES </button>
+
+    </div>
+  );
+
+}
+
+export default App;
+
+
+
